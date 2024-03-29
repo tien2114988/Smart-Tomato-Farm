d94da34a0f2a47330559f5ce9a98082d4ebd332d
KhaiToan1604 <toan.trinh1604@hcmut.edu.vn>
2024-03-29 21:22:17 +0700

    Menu added

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,19 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header/Header';
+import Menu from './components/Menu/Menu';
+import Footer from './components/Footer/Footer';
 import Manage from './features/Manage/Manage';
 import {Route, Routes} from 'react-router-dom';
-import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className='App'>
-      <Header/>
-      <Sidebar/>
+      <Menu/>
+      
       <Routes>
         <Route path='/' element={<Manage/>}/>
         <Route path='/manage' element={<Manage/>}/>
       </Routes>
+      
+      <Footer/>
     </div>
   );
 }
---
+++ ui/src/components/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import { Toolbar, Typography } from "@mui/material";
+
+export default function Footer() {
+  return (
+    <Toolbar
+      sx={{
+        backgroundColor: "#023556",
+        // flexShrink: 0,
+        color: "#fff",
+        fontSize: "16px",
+        // bottom: "64px",
+        // width: "100vw",
+        // position: "relative",
+      }}
+    >
+      <Typography sx={{ marginLeft: "7%" }}>
+        Copyright-2024-Trường Đại học Bách Khoa TP.HCM
+      </Typography>
+    </Toolbar>
+  );
+}

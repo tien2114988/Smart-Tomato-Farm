9b048e1e862bb695aff73d8245811b204ef560c1
ptho1504 <phuoctho150420@gmail.com>
2024-03-31 13:29:48 +0700

    reafactor(light):add logic tab

--- ui/src/features/Light/Light.jsx
+++ ui/src/features/Light/Light.jsx
@@ -2,47 +2,49 @@ import React, { useState } from "react";
 import "./Light.css";
 import Bulb from "./components/Bulb";
 import SettingBulb from "./components/SettingBulb";
+import Area from "./pages/Area";
 
-const bulbs = [
-  {
-    id: 1,
-    name: "Đèn 1",
-    state: true,
-  },
-  {
-    id: 1,
-    name: "Đèn 2",
-    state: false,
-  },
-  {
-    id: 1,
-    name: "Đèn 3",
-    state: true,
-  },
-];
 const areas = [
   {
     id: 1,
-    name: "Khu A"
+    name: "Khu A",
   },
   {
     id: 2,
-    name: "Khu B"
-  }
-]
+    name: "Khu B",
+  },
+];
 const Light = () => {
-  const [areaA, setAreaA] = useState(true);
+  const [chooseArea, setChooseArea] = useState(areas[0]);
   const [setting, setSetting] = useState(false);
+  const handleTabs = (id) => {
+    areas.forEach((area) => {
+      if (area.id === id) {
+        setChooseArea(area);
+      }
+    });
+  };
   return (
     <div className="Light border p-2 w-100">
       <div className="tabs w-100 h-auto d-flex gap-2 mb-5 justify-content-between align-items-center">
         <div className="areas d-flex gap-5 fs">
-          <div className={`area h4 m-0 px-4 py-2 ${areaA ? "active" : ""}`}>
+          {areas.map((area) => (
+            <div
+              key={area.id}
+              className={`area h4 m-0 px-4 py-2 ${
+                chooseArea.name === area.name ? "active" : ""
+              }`}
+              onClick={() => handleTabs(area.id)}
+            >
+              {area.name}
+            </div>
+          ))}
+          {/* <div className={`area h4 m-0 px-4 py-2 ${areaA ? "active" : ""}`}>
             Khu A
           </div>
           <div className={`area h4 m-0 px-4 py-2 ${areaA ? "" : "active"}`}>
             Khu B
-          </div>
+          </div> */}
         </div>
         <div className="setting">
           <button
@@ -53,18 +55,7 @@ const Light = () => {
           </button>
         </div>
       </div>
-      <div className="content px-4 d-flex flex-row gap-5 flex-wrap justify-content-between">
-        {bulbs.map((bulb) => (
-          <Bulb key={bulb.id} name={bulb.name} state={bulb.state} />
-        ))}
-      </div>
-      {/* {setting ? (
-        <SettingBulb
-          setting={setting}
-          handleSetting={() => setSetting(!setting)}
-          
-        />
-      ) : null} */}
+      <Area area={chooseArea} key={chooseArea.id}/>
       <SettingBulb
         setting={setting}
         handleSetting={() => setSetting(!setting)}

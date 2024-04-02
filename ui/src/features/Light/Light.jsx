f14a41a5e4ab9aa8359cbf3b002c4d054b837d99
ptho1504 <phuoctho150420@gmail.com>
2024-04-02 13:37:48 +0700

    fix(light): Update UI light

--- ui/src/features/Light/Light.jsx
+++ ui/src/features/Light/Light.jsx
@@ -1,39 +1,62 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Light.css";
 import SettingBulb from "./components/SettingBulb";
 import Area from "./pages/Area";
+import getAllArea from "../../api/areaApi";
+import axios from "axios";
 
-const areas = [
-  {
-    id: 1,
-    name: "Khu A",
-  },
-  {
-    id: 2,
-    name: "Khu B",
-  },
-];
+// const areas = [
+//   {
+//     id: 1,
+//     name: "Khu A",
+//   },
+//   {
+//     id: 2,
+//     name: "Khu B",
+//   },
+// ];
+// console.log(typeof(areas));
 const Light = () => {
-  const [chooseArea, setChooseArea] = useState(areas[0]);
+  const [areas, setAreas] = useState([]);
+  const [chooseArea, setChooseArea] = useState(null);
   const [setting, setSetting] = useState(false);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/api/light/")
+      .then((res) => {
+        // console.log(res);
+        setAreas(res.data);
+        if (res.data.length > 0) {
+          setChooseArea(res.data[0]);
+        }
+        setLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   const handleTabs = (id) => {
     areas.forEach((area) => {
-      if (area.id === id) {
+      if (area._id === id) {
         setChooseArea(area);
       }
     });
   };
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
   return (
     <div className="Light border p-2 w-100">
       <div className="tabs w-100 h-auto d-flex gap-2 mb-5 justify-content-between align-items-center">
         <div className="areas d-flex gap-5 fs">
           {areas.map((area) => (
             <div
-              key={area.id}
+              key={area._id}
               className={`area h4 m-0 px-4 py-2 ${
                 chooseArea.name === area.name ? "active" : ""
               }`}
-              onClick={() => handleTabs(area.id)}
+              onClick={() => handleTabs(area._id)}
             >
               {area.name}
             </div>
@@ -54,7 +77,7 @@ const Light = () => {
           </button>
         </div>
       </div>
-      <Area area={chooseArea} key={chooseArea.id}/>
+      <Area area={chooseArea} key={chooseArea._id} />
       <SettingBulb
         setting={setting}
         handleSetting={() => setSetting(!setting)}

0b392661ece9e372f4d4740266a564e7a9de8267
KhaiToan1604 <toan.trinh1604@hcmut.edu.vn>
2024-04-15 20:18:54 +0700

    add static ui

--- ui/src/App.js
+++ ui/src/App.js
@@ -25,7 +25,7 @@ const router = createBrowserRouter([
         element: <Manage />,
       },
       {
-        path: "light",
+        path: "/light",
         element: <Light />,
       },
       {

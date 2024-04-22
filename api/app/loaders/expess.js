a97c22623056d41e72482c0c5845fad0042cca2e
ptho1504 <phuoctho150420@gmail.com>
2024-04-22 11:21:04 +0700

    Update: folder

--- api/app/loaders/expess.js
+++ api/app/loaders/expess.js
@@ -3,14 +3,18 @@ const logger = require("morgan");
 const bodyPraser = require("body-parser");
 const cors = require("cors");
 const lightRouter = require("../routes/light.route.js");
+const manageRouter = require("../routes/manage.route.js");
+const analysisRouter = require("../routes/analysis.route.js");
 
 async function expressLoader({ app }) {
   app.use(logger("dev"));
   app.use(bodyPraser.json());
   app.use(cors());
 
-  app.use("/api/light", lightRouter);
 
+  app.use("/api/manage", manageRouter);
+  app.use("/api/light", lightRouter);
+  app.use("/api/analysis",analysisRouter);
   //catch 404 error
   app.use((req, res, next) => {
     const err = new Error("Not Found");

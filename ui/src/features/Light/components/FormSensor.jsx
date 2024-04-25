111e55cc75ad8a064c217d2269e66a933d553aad
ptho1504 <phuoctho150420@gmail.com>
2024-04-25 10:27:23 +0700

    f

--- ui/src/features/Light/components/FormSensor.jsx
+++ ui/src/features/Light/components/FormSensor.jsx
@@ -1,19 +1,52 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-const FormSensor = ({ props }) => {
-  const lux = props;
+const FormSensor = ({
+  bulb,
+  levelOne,
+  levelTwo,
+  auto,
+  handleSetting,
+  setAuto,
+}) => {
+  const [light, setLight] = useState(null);
+  // console.log(auto);
+  // console.log("Bulb", bulb);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await axios
+      .put(`http://localhost:3001/api/light/bulbs/setting`, {
+        bulb_id: bulb._id,
+        mode: "sensor",
+        status: true,
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    setAuto((prevAuto) => {
+      return {
+        ...prevAuto,
+        appliedTimer: false,
+        appliedSensor: true,
+      };
+    });
+    handleSetting();
+  };
   return (
     <div>
-      <form action="" className="d-flex flex-column gap-2">
-        <label>Đèn bật khi</label>
+      <form className="d-flex flex-column gap-2">
+        <label> Bật Hai đèn khi</label>
         <select>
-          <option value="">Khi cường đó ánh sáng &lt; {lux} lux</option>
+          <option value="">Khi cường đó ánh sáng &lt; {levelOne} %</option>
         </select>
-        <label>Đèn tắt khi</label>
+        <label>Bật Một đèn khi</label>
         <select>
-          <option value="">Khi cường đó ánh sáng &gt; {lux} lux</option>
+          <option value="">Khi cường đó ánh sáng &lt; {levelTwo} %</option>
         </select>
-        <button className="automode__save border">Lưu</button>
+        <button className="automode__save border" onClick={onSubmit}>
+          Lưu
+        </button>
       </form>
     </div>
   );
